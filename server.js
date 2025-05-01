1004d38940eaadb06b077df9fd9c2b0cf9449383
mail4client <forclientm@gmail.com>
2025-05-01 11:58:39 +0530

    Update server.js

--- server.js
+++ server.js
@@ -92,8 +92,8 @@ app.post('/index', async (req, res) => {
       }
     });
 
-    console.log(process.env.MAIL_USER)  // Debugging line to check if the environment variable is set
-    console.log(process.env.MAIL_PASS)  // Debugging line to check if the environment variable is set
+    //console.log(process.env.MAIL_USER)  // Debugging line to check if the environment variable is set
+    //console.log(process.env.MAIL_PASS)  // Debugging line to check if the environment variable is set
 
     const mailOptions = {
       from: email,
